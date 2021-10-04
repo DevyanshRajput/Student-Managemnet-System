fa8991ecaac932fdcc2e7791cd631bf55027c77e
devya <devya@DESKTOP-OBAPSO0>
2021-10-04 14:51:48 +0530

    SMSystem

---
+++ Student Management System/src/smsystem/general/DatabaseConnection.java
@@ -0,0 +1,68 @@
+package smsystem.general;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * Title : DataBaseConnection.java
+ * Purpose : For database connection
+ */
+
+public class DatabaseConnection {
+	
+	static Connection con=null;
+	static final String url="jdbc:mysql://localhost:3306/studentmanagement";
+	static final String uname="root";
+	static final String password="";
+	
+	
+	public static Connection getConnection()
+	{
+		if(con!=null)
+		{
+			return con;
+		}
+		else
+		{
+			try
+			{
+				
+				
+				con=DriverManager.getConnection(url,uname,password);
+				return con;
+			}
+			catch(Exception exp)
+			{
+				exp.printStackTrace();
+				return con;
+			}
+		}
+	}
+	public static boolean checkconnection() 
+	{
+		
+		try
+		{
+			
+	
+			con=DriverManager.getConnection(url,uname,password);
+			return true;
+		}
+		catch(Exception exp)
+		{
+			exp.printStackTrace();
+			return false;
+		}
+	}
+	public static void  closeConnection() 
+	{
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
